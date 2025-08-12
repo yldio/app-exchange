705d712788ca26c2e548f95289b156f99870dedb
OleksiiM <omblinda@gmail.com>
2025-08-12 15:13:41 +0100

    Add celo test

--- test/python/apps/cal.py
+++ test/python/apps/cal.py
@@ -3,7 +3,7 @@ from ledger_app_clients.exchange.cal_helper import CurrencyConfiguration
 from .sui_utils import SUI_CONF, SUI_PACKED_DERIVATION_PATH, SUI_USDC_CONF
 
 # Eth family
-from .ethereum import ETH_PACKED_DERIVATION_PATH, ETH_CONF
+from .ethereum import CELO_CONF, CELO_PACKED_DERIVATION_PATH, ETH_PACKED_DERIVATION_PATH, ETH_CONF
 from .ethereum import ETC_PACKED_DERIVATION_PATH, ETC_CONF
 from .ethereum import BSC_PACKED_DERIVATION_PATH, BSC_CONF, BSC_CONF_LEGACY
 from .ethereum import DAI_PACKED_DERIVATION_PATH, DAI_CONF
@@ -38,6 +38,7 @@ ETC_CURRENCY_CONFIGURATION = CurrencyConfiguration(ticker="ETC", conf=ETC_CONF, 
 BNB_CURRENCY_CONFIGURATION = CurrencyConfiguration(ticker="BNB", conf=BSC_CONF, packed_derivation_path=BSC_PACKED_DERIVATION_PATH)
 BNB_LEGACY_CURRENCY_CONFIGURATION = CurrencyConfiguration(ticker="BNB", conf=BSC_CONF_LEGACY, packed_derivation_path=BSC_PACKED_DERIVATION_PATH)
 DAI_CURRENCY_CONFIGURATION = CurrencyConfiguration(ticker="DAI", conf=DAI_CONF, packed_derivation_path=DAI_PACKED_DERIVATION_PATH)
+CELO_CURRENCY_CONFIGURATION = CurrencyConfiguration(ticker="CELO", conf=CELO_CONF, packed_derivation_path=CELO_PACKED_DERIVATION_PATH)
 
 # --8<-- [start:sol_conf]
 # Solana and Solana tokens
